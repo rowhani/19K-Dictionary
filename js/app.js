3086063889a31329ead1bb026b14d8f1d1cdd418
Payman Rowhani <payman.rowhani@medtelligent.com>
2015-01-09 22:12:25 +0330

    Final changes

--- js/app.js
+++ js/app.js
@@ -5,43 +5,43 @@ angular.module('19kdic', ['ionic', '19kdic.utils', '19kdic.controllers'])
     if (window.StatusBar) {
       StatusBar.styleDefault();
     }
-	
-		function loadFile(path, callback) {
-			$.ajax({
-				url : path,
-				dataType: "text",
-				success : function (data) {
-					callback(data)
-				}
-			});
-		}
 
-		function createDB() {
-			var records = [];
-			$.each($rootScope.words, function(i, k) {  
-			  records.push({
-				  id: i,
-				  key: common.getCleanQuery($rootScope.words[i]),
-				  word: common.getRawQuery($rootScope.words[i]),
-				  meaning: $.trim($rootScope.meanings[i])
-			  });
-			});
-			
-			$rootScope.db = TAFFY(records);
-			console.log("DB Loaded");
-		}
+    function loadFile(path, callback) {
+      $.ajax({
+        url: path,
+        dataType: "text",
+        success: function(data) {
+          callback(data)
+        }
+      });
+    }
 
-		function initialize() {
-			loadFile("../db/words.txt", function (data) {
-				$rootScope.words = data.split("\n");
-				loadFile("../db/meanings.txt", function (data) {
-					$rootScope.meanings = data.split("\n");
-					createDB();
-				})
-			});
-		}
-		
-		initialize();	
+    function createDB() {
+      var records = [];
+      $.each($rootScope.words, function(i, k) {
+        records.push({
+          id: i,
+          key: common.getCleanQuery($rootScope.words[i]),
+          word: common.getRawQuery($rootScope.words[i]),
+          meaning: $.trim($rootScope.meanings[i])
+        });
+      });
+
+      $rootScope.db = TAFFY(records);
+      console.log("DB Loaded");
+    }
+
+    function initialize() {
+      loadFile("../db/words.txt", function(data) {
+        $rootScope.words = data.split("\n");
+        loadFile("../db/meanings.txt", function(data) {
+          $rootScope.meanings = data.split("\n");
+          createDB();
+        })
+      });
+    }
+
+    initialize();
   });
 })
 
@@ -54,26 +54,26 @@ angular.module('19kdic', ['ionic', '19kdic.utils', '19kdic.controllers'])
     templateUrl: "templates/app.html",
     controller: 'AppCtrl'
   })
-  
+
   .state('app.search', {
     url: "/search",
-	views: {
+    views: {
       'appContent': {
-	    templateUrl: "templates/search.html",
-		controller: 'SearchCtrl'
-	  }
-	}
+        templateUrl: "templates/search.html",
+        controller: 'SearchCtrl'
+      }
+    }
   })
-  
+
   .state('app.definition', {
     url: "/search/:wordId",
     views: {
       'appContent': {
-	    templateUrl: "templates/definition.html",
-		controller: 'DefinitionCtrl'
-	  }
-	}
+        templateUrl: "templates/definition.html",
+        controller: 'DefinitionCtrl'
+      }
+    }
   })
-    
+
   $urlRouterProvider.otherwise('/app/search');
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,86 +1,86 @@
 ﻿angular.module('19kdic.controllers', [])
 
 .controller('AppCtrl', function($scope, common) {
-  console.log("App");  
+  console.log("App");
 })
 
 .controller('SearchCtrl', function($scope, $timeout, common) {
-	console.log("Search");	
-	
-	$scope.searchData = {
-		query : ''
-	}
-  
-	$scope.clearSearch = function() {
-		$scope.searchData.query = '';
-		$scope.letters = [];
-		$scope.results = {};
-		$scope.searching = false;
-	};
-	
-	$scope.clearSearch();
-	
-	function search() {
-		console.log("Searching...");
-		var results = common.lookup($scope.searchData.query);
-		$.each(results, function(i, r) {
-			var ch = r.word.charAt(0);
-			if ($scope.letters.indexOf(ch) == -1) {
-				$scope.letters.push(ch);
-				$scope.results[ch] = [];
-			}
-			$scope.results[ch].push(r);
-		});		
-		$scope.searchTimeout = null;
-		$scope.searching = false;
-		$scope.$apply();
-		console.log("Results:", $scope.searchData.query, results.length);
-	}
-	
-	$scope.search = function($event) {
-		$scope.letters = [];
-		$scope.results = {};
-			
-		if (!$scope.searchData.query.length) {
-			$scope.searching = false;
-			$scope.$apply();
-			return;
-		}
-		
-		$scope.searching = true;
-		$scope.$apply();
-		if ($scope.searchTimeout){
-			window.clearTimeout($scope.searchTimeout);
-		}  
-		$scope.searchTimeout = setTimeout(search, 500);
-	};
-	
-	$scope.highlight = function(el) {
-		$timeout(function(){
-			var cleanQuery = common.getCleanQuery($scope.searchData.query);
-			var rawQuery = common.getRawQuery($scope.searchData.query);
-			$(".result").highlight([cleanQuery, rawQuery]);
-		}, 0);
-	};
+  console.log("Search");
+
+  $scope.searchData = {
+    query: ''
+  };
+
+  $scope.clearSearch = function() {
+    $scope.searchData.query = '';
+    $scope.letters = [];
+    $scope.results = {};
+    $scope.searching = false;
+  };
+
+  $scope.clearSearch();
+
+  function search() {
+    console.log("Searching...");
+    var results = common.lookup($scope.searchData.query);
+    $.each(results, function(i, r) {
+      var ch = r.word.charAt(0);
+      if ($scope.letters.indexOf(ch) == -1) {
+        $scope.letters.push(ch);
+        $scope.results[ch] = [];
+      }
+      $scope.results[ch].push(r);
+    });
+    $scope.searchTimeout = null;
+    $scope.searching = false;
+    $scope.$apply();
+    console.log("Results:", $scope.searchData.query, results.length);
+  }
+
+  $scope.search = function($event) {
+    $scope.letters = [];
+    $scope.results = {};
+
+    if (!common.getCleanQuery($scope.searchData.query).length) {
+      $scope.searching = false;
+      $scope.$apply();
+      return;
+    }
+
+    $scope.searching = true;
+    $scope.$apply();
+    if ($scope.searchTimeout) {
+      window.clearTimeout($scope.searchTimeout);
+    }
+    $scope.searchTimeout = setTimeout(search, 500);
+  };
+
+  $scope.highlight = function(el) {
+    $timeout(function() {
+      var cleanQuery = common.getCleanQuery($scope.searchData.query);
+      var rawQuery = common.getRawQuery($scope.searchData.query);
+      $(".result").highlight([cleanQuery, rawQuery]);
+    }, 0);
+  };
 })
 
 .controller('DefinitionCtrl', function($scope, $rootScope, $state, $stateParams, common) {
-	console.log("Definition:", $stateParams.wordId);
-	
-	  $scope.back = function() {
-		$state.go('app.search');
-	  };
-	
-	$scope.word = common.getWord($stateParams.wordId);
-	
-	var meanings = common.getMeanings($stateParams.wordId);	
-	$scope.meanings = [];
-	$.each(meanings, function() {
-		var parts = [];
-		$.each(this.split(" "), function() {
-			var meaning = $.trim(this.replace(")", " ").replace("(", " ").replace("»", " ").replace("«", " ").replace("»", " ").replace("  ", " "));
-			parts.push(this.replace(meaning, common.getWordLink(meaning, $stateParams.wordId)));
-		});
-		$scope.meanings.push(parts.join(" "));
-	});
+  console.log("Definition:", $stateParams.wordId);
+
+  $scope.back = function() {
+    $state.go('app.search');
+  };
+
+  $scope.word = common.getWord($stateParams.wordId);
+
+  var meanings = common.getMeanings($stateParams.wordId);
+  $scope.meanings = [];
+  $.each(meanings, function() {
+    var parts = [];
+    $.each(this.split(" "), function() {
+      var meaning = $.trim(this.replace(")", " ").replace("(", " ").replace("»", " ").replace("«", " ").replace("»", " ").replace("  ", " "));
+      parts.push(this.replace(meaning, common.getWordLink(meaning, $stateParams.wordId)));
+    });
+    $scope.meanings.push(parts.join(" "));
+  });
 })
